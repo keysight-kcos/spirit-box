47872a22e3af1d87dd81f292032ebd84cd2fea0e
Lorenzo <56091505+lgardea@users.noreply.github.com>
2022-06-28 12:12:18 -0700

    updates to main

--- scripts/scripts.go
+++ scripts/scripts.go
@@ -8,27 +8,31 @@ import(
 	"os"
 	"errors"
 	"bufio"
+	"spirit-box/logging"
 )
 
-func RunScripts(){
+func RunAllScripts() {
 	runScriptsInDir()
 	loadScriptList()
 }
 
 func runScriptsInDir(){
+	l := logging.Logger
 	scriptDir := "/usr/share/spirit-box/"
 	items, _ := ioutil.ReadDir(scriptDir)
-	fmt.Println("scripting...");
-	for _, item := range items{
+	fmt.Printf("Running scripts in %s\n", scriptDir);
+	for _, item := range items {
 		if !item.IsDir() && item.Name()[len(item.Name())-3:] == ".sh"{
-			fmt.Println("running script " + scriptDir + item.Name())
+			fmt.Println("Running script " + scriptDir + item.Name() + "...")
 			out, err := exec.Command("/bin/sh", scriptDir + item.Name()).Output()
 			if err != nil{
 				log.Fatal(err)
 			}
 			fmt.Printf("%s", out)
+			l.Printf("Ran %s%s", scriptDir, item.Name())
 		}
 	}
+	fmt.Println()
 }
 
 func loadScriptList() ([]string, error) {
@@ -38,27 +42,32 @@ func loadScriptList() ([]string, error) {
 		fmt.Println("No script file.")
 		return lines, err
 	}
+
+	fmt.Printf("Running scripts based on path names in %s\n", path)
 	file, err := os.Open(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
+	l := logging.Logger
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
 		if _, err := os.Stat(line); errors.Is(err, os.ErrNotExist) {
 			log.Fatal(errors.New("Script does not exist: " + line))
 		} else {
-			fmt.Println("running script " + line)
+			fmt.Println("Running script " + line + "...")
 			lines = append(lines, line)
 			out, err := exec.Command("/bin/sh", line).Output()
-			if err != nil{
+			if err != nil {
 				log.Fatal(err)
 			}
 			fmt.Printf("%s", out)
+			l.Printf("Ran %s", line)
 		}
 
 	}
+	fmt.Println()
 	return lines, scanner.Err()
 }
